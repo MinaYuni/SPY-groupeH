5e4bf253fd9dd4d5a22c1f7662efc6df50df1001
zlouised <averyfall@AveryFall.localdomain>
2023-01-09 22:45:36 +0100

    popups kinda work but only once per level cuz of boolean

--- Assets/AutomaticScript/TriggerSystem_wrapper.cs
+++ Assets/AutomaticScript/TriggerSystem_wrapper.cs
@@ -3,9 +3,16 @@ using FYFY;
 
 public class TriggerSystem_wrapper : BaseWrapper
 {
+	public UnityEngine.GameObject dialogPanel;
 	private void Start()
 	{
 		this.hideFlags = HideFlags.NotEditable;
+		MainLoop.initAppropriateSystemField (system, "dialogPanel", dialogPanel);
+	}
+
+	public void setActiveOKButton(System.Boolean active)
+	{
+		MainLoop.callAppropriateSystemMethod (system, "setActiveOKButton", active);
 	}
 
 }
--- Assets/Systems/TriggerSystem.cs
+++ Assets/Systems/TriggerSystem.cs
@@ -1,13 +1,19 @@
 using UnityEngine;
 using FYFY;
+using TMPro;
+using UnityEngine.UI;
+using System.IO;
+using System.Collections;
+using UnityEngine.Networking;
+using System;
 
 public class TriggerSystem : FSystem {
 	private Family f_players = FamilyManager.getFamily(new AnyOfTags("Player"));
-
 	public static TriggerSystem instance;
-// fam manager to get agent edit then allCompononents
-// 
 	private GameData gameData = FamilyManager.getFamily(new AllOfComponents(typeof(GameData))).First().GetComponent<GameData>();
+	public GameObject dialogPanel;
+	private bool popped;
+	// private Tuple curr_pos;
 
 	public TriggerSystem()
 	{
@@ -16,12 +22,16 @@ public class TriggerSystem : FSystem {
 	
 	// Use to init system before the first onProcess call
 	protected override void onStart(){
+		GameObjectManager.setGameObjectState(dialogPanel.transform.parent.gameObject, false);
+		popped = false;
+		// curr_pos = (0,0,0);
+
 
 	}
 
 	// Use to process your families.
 	protected override void onProcess(int familiesUpdateCount) {
-		Debug.Log("hey we are in the onProcess");
+		// Debug.Log("hey we are in the onProcess");
 		foreach(GameObject go in f_players){
 			
 			var pos = go.GetComponent<Position>();
@@ -29,11 +39,45 @@ public class TriggerSystem : FSystem {
 			var key = (pos.x, pos.y, (int)direction.direction);
 			
 			if(gameData.triggerMessage.ContainsKey(key)){
-				Debug.Log(gameData.triggerMessage[key]);
+				// Debug.Log(gameData.triggerMessage[key]);
+				
+				configureDialog(gameData.triggerMessage[key]);
+				popped = true;
 
 			}
 			
-			
 		}
+	}
+
+	private void configureDialog(string popup){
+		// set text
+		if(popped == false){
+			GameObjectManager.setGameObjectState(dialogPanel.transform.parent.gameObject, true);
+			GameObject textGO = dialogPanel.transform.Find("Text").gameObject;
+			// if (gameData.triggerMessage[nDialog].Item1 != null)
+			// {
+			GameObjectManager.setGameObjectState(textGO, true);
+			textGO.GetComponent<TextMeshProUGUI>().text = popup;
+			LayoutRebuilder.ForceRebuildLayoutImmediate(textGO.transform as RectTransform);
+			// }
+			// else
+			// 	GameObjectManager.setGameObjectState(textGO, false);
+			
+			// set camera pos
+			// if (gameData.dialogMessage[nDialog].Item4 != -1 && gameData.dialogMessage[nDialog].Item5 != -1)
+			// {
+			// 	GameObjectManager.addComponent<FocusCamOn>(MainLoop.instance.gameObject, new { camX = gameData.dialogMessage[nDialog].Item4, camY = gameData.dialogMessage[nDialog].Item5 });
+			// }
+
+			setActiveOKButton(true);
+		}
+
+	}
+	public void setActiveOKButton(bool active){
+		GameObjectManager.setGameObjectState(dialogPanel.transform.Find("Buttons").Find("OKButton").gameObject, active);
+	}
+
+	public void closeDialogPanel(){
+		GameObjectManager.setGameObjectState(dialogPanel.transform.parent.gameObject, false);
 	}
 }
